5d38bf4bc829b79be68730530ed22a5274c130a7
kharg <karol@maciaszek.pl>
2010-07-20 15:38:36 +0000

    - rozdzielenie kodu firmware na dwa urządzenia - iochip i ledchip

--- firmware/ledchip/dev/rgbled.h
+++ firmware/ledchip/dev/rgbled.h
@@ -24,7 +24,7 @@ void rgbled_set_colour(uint8_t led_no, rgbled_colour* colour);
 
 void rgbled_refresh();
 
-void rgbled_init(void (*func)(void));
+void rgbled_init(void);
 
 
 #endif /* RGBLED_H_ */
